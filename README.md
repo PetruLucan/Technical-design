a7f1e0741242a3f2540e82cfee0900747ff7b930
PetruLucan <lucanpetru11@gmail.com>
2023-06-11 16:38:16 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# technical design
+ 
---
+++ script.js
@@ -0,0 +1,39 @@
+// const servicii = document.getElementById('dropdown-toggle');
+// const list = document.getElementById('dropdown-options');
+
+// list.style.display = "none";
+
+// servicii.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   if (list.style.display === "none") {
+//     list.style.display = "flex";
+//     servicii.classList.add("clicked");
+//   } else {
+//     list.style.display = "none";
+//     servicii.classList.remove("clicked");
+//   }
+// });
+
+// document.addEventListener("click", (event) => {
+//   if (event.target !== servicii) {
+//     list.style.display = "none";
+//     servicii.classList.remove("clicked");
+//   }
+// });
+
+
+const dropdownToggle = document.getElementById('dropdown-toggle');
+const dropdownOptions = document.getElementById('dropdown-options');
+const dropdownParent = dropdownToggle.closest('.dropdown');
+
+dropdownToggle.addEventListener('click', function(event) {
+  event.stopPropagation(); // Prevent the click event from bubbling up to the document
+  dropdownParent.classList.toggle('show');
+});
+
+document.addEventListener('click', function(event) {
+  if (!dropdownParent.contains(event.target)) {
+    dropdownParent.classList.remove('show');
+  }
+});
+
